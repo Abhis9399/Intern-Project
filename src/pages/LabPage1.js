4dcd26cc0eadae46c22d029ab3f09abdfd37ac5a
Abhis9399 <139974070+Abhis9399@users.noreply.github.com>
2023-08-13 12:24:12 +0530

    Add files via upload

---
+++ src/pages/LabPage1.js
@@ -0,0 +1,414 @@
+import styles from "./LabPage1.module.css";
+const LabPage1 = () => {
+  return (
+    <div className={styles.labPage1}>
+      <div className={styles.bg}>
+        <div className={styles.bgChild} />
+        <img className={styles.bgItem} alt="" src="/rectangle-63.svg" />
+        <div className={styles.notinline}>NotInLine</div>
+        <div className={styles.notinline1}>NotInLine</div>
+        <div className={styles.notinline2}>NotInLine</div>
+        <img className={styles.bgInner} alt="" src="/rectangle-82.svg" />
+        <img className={styles.rectangleIcon} alt="" src="/rectangle-22.svg" />
+        <img
+          className={styles.expressiveYoungWomanPosingIcon}
+          alt=""
+          src="/expressiveyoungwomanposingstudioremovebgpreviewtransformed-1@2x.png"
+        />
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerChild} />
+        <img
+          className={styles.nilLogoTransparent11}
+          alt=""
+          src="/nil-logo-transparent-1-1@2x.png"
+        />
+        <div className={styles.bookDoctorAppointments}>
+          Book doctor appointments online and get your ailments treated in no
+          time with our expert medical support systems. Get rid of all your
+          medical problem with our expert panel of doctors who guide you towards
+          a healthier life.
+        </div>
+        <div className={styles.appointments}>Appointments</div>
+        <div className={styles.footerItem} >
+          <input  type="tel" className={styles.footerFeedback} name="feedbackCall" id="feedbackCall" />
+        </div>
+        <button style={{cursor:'pointer'}}>
+        <img className={styles.footerInner} alt="" src="/rectangle-51.svg" />
+        <img className={styles.callIcon} alt="" src="/call.svg" />
+        </button>
+        <div className={styles.lineDiv} />
+        <div className={styles.div}>+91</div>
+        <div className={styles.services}>Services</div>
+        <div className={styles.labTests}>Lab tests</div>
+        <div className={styles.aZMedicine}>A-Z medicine</div>
+        <div className={styles.doctorSupport}>Doctor support</div>
+        <div className={styles.generalInfo}>General info</div>
+        <div className={styles.legal}>Legal</div>
+        <div className={styles.privacyPolicy}>Privacy policy</div>
+        <div className={styles.termsOfServices}>Terms of services</div>
+        <div className={styles.consultation}>Consultation</div>
+        <div className={styles.howItWorks}>How it works</div>
+        <div className={styles.supportnotinlinecom}>support@notinline.com</div>
+        <div className={styles.talkToUs}>Talk to us</div>
+        <div className={styles.appointmentnotinlinecom}>
+          appointment@notinline.com
+        </div>
+        <div className={styles.div1}>+91 12345 67899</div>
+        <div className={styles.div2}>+91 97642 09752</div>
+        <div className={styles.allRightsReserved}>
+          2021-2023 All Rights Reserved
+        </div>
+        <div className={styles.component4}>
+          <div className={styles.component4Child} />
+          <div className={styles.c}>C</div>
+        </div>
+        <div className={styles.footerChild1} />
+      </div>
+      <div className={styles.heroTextImage}>
+        <div className={styles.component1}>
+          <div className={styles.component1Child} />
+          <div className={styles.component1Item} />
+          <img
+            className={styles.mediumShotWomanLookingThroIcon}
+            alt=""
+            src="/mediumshotwomanlookingthroughmicroscoperemovebgpreview-1@2x.png"
+          />
+          <div className={styles.component1Inner} />
+          <div className={styles.rectangleDiv} />
+        </div>
+        <div className={styles.component7}>
+          <div className={styles.component7Child} />
+          <img
+            className={styles.component7Item}
+            alt=""
+            src="/ellipse-1@2x.png"
+          />
+        </div>
+        <div className={styles.component5}>
+          <div className={styles.component7Child} />
+          <div className={styles.complementaryReportConsultat}>
+            Complementary report consultation
+          </div>
+          <img
+            className={styles.component5Item}
+            alt=""
+            src="/ellipse-2@2x.png"
+          />
+        </div>
+        <div className={styles.component6}>
+          <div className={styles.component7Child} />
+          <img
+            className={styles.component6Item}
+            alt=""
+            src="/ellipse-3@2x.png"
+          />
+        </div>
+        <div className={styles.bestInClassContainer}>
+          <span>Best in class</span>
+          <span className={styles.labTests1}> lab tests!</span>
+        </div>
+        <img className={styles.heroTextImageChild} alt="" src="/vector-1.svg" />
+        <div className={styles.accurateTimelyReports}>
+          <div className={styles.accurateTimelyReports1}>
+            Accurate timely reports
+          </div>
+        </div>
+        <div className={styles.timelySampleCollection}>
+          <div className={styles.accurateTimelyReports1}>
+            Timely sample collection
+          </div>
+        </div>
+      </div>
+      <div className={styles.fillInForm}>
+        <div className={styles.fillInFormChild} />
+        <div className={styles.fillInFormItem} />
+        <div className={styles.fillInFormInner} />
+        <div className={styles.fillInFormChild1} />
+        <input
+    type="text"
+    className={styles.fullName}
+    placeholder="Full name"
+  />
+  <input
+    type="tel"
+    className={styles.phoneNumber}
+    placeholder="Phone number"
+  />
+  <input
+    type="text"
+    className={styles.location}
+    placeholder="Location"
+  />
+        <img className={styles.personIcon} alt="" src="/person.svg" />
+        <img className={styles.callIcon1} alt="" src="/call1.svg" />
+        <img className={styles.distanceIcon} alt="" src="/distance.svg" />
+        <input type="checkBox" className={styles.component2Icon}  />
+        <div className={styles.byContinuingYouContainer}>
+          <span className={styles.byContinuingYou}>
+            By continuing, you agree to our
+          </span>
+          <span className={styles.tc}>{` T&C`}</span>
+          <span className={styles.byContinuingYou}>{` and `}</span>
+          <span className={styles.tc}>privacy policy</span>
+        </div>
+        <button className={styles.fillInFormChild2} /> 
+        <button className={styles.proceed}>Proceed</button>
+        <div className={styles.fillInThe}>
+          Fill in the details to get a call!
+        </div>
+      </div>
+      <div className={styles.offerLine}>
+        <div className={styles.offerLineChild} />
+        <div className={styles.offerLineItem} />
+        <div className={styles.getAFullContainer}>
+          <span>Get a Full Body Checkup</span>
+          <span className={styles.tc}>{` Now `}</span>
+          <span>at</span>
+        </div>
+        <img
+          className={styles.currencyRupeeIcon}
+          alt=""
+          src="/currency-rupee.svg"
+        />
+        <div className={styles.div3}>5,999</div>
+        <div className={styles.offerLineInner} />
+        <img
+          className={styles.currencyRupeeIcon1}
+          alt=""
+          src="/currency-rupee1.svg"
+        />
+        <div className={styles.div4}>999</div>
+        <div className={styles.off}>70% OFF</div>
+      </div>
+      <div className={styles.whyChooseUs}>
+        <div className={styles.whyChooseUsContainer}>
+          <span>{`Why `}</span>
+          <span className={styles.choose}>choose</span>
+          <span> us?</span>
+        </div>
+        <div className={styles.weWantYou}>
+          We want you and your heart to be healthy so you can live a happy life!
+        </div>
+        <img className={styles.whyChooseUsChild} alt="" src="/vector-4.svg" />
+        <div className={styles.whyChooseUsItem} />
+        <div className={styles.whyChooseUsInner} />
+        <div className={styles.whyChooseUsChild1} />
+        <div className={styles.whyChooseUsChild2} />
+        <img className={styles.ellipseIcon} alt="" src="/ellipse-14@2x.png" />
+        <img
+          className={styles.whyChooseUsChild3}
+          alt=""
+          src="/ellipse-13@2x.png"
+        />
+        <img
+          className={styles.whyChooseUsChild4}
+          alt=""
+          src="/ellipse-15@2x.png"
+        />
+        <div className={styles.nablAccreditedLabs}>NABL Accredited Labs</div>
+        <div className={styles.valueExperience}>Value experience</div>
+        <div className={styles.timelyCollections}>Timely collections</div>
+        <div className={styles.transparency}>Transparency</div>
+        <img
+          className={styles.whyChooseUsChild5}
+          alt=""
+          src="/ellipse-16@2x.png"
+        />
+        <div
+          className={styles.getAccurateAnd}
+        >{`Get accurate and safe results from our NABL-certified lab partners.  `}</div>
+        <div className={styles.experienceSeamlessCare}>
+          Experience seamless care without the hassle of waiting in long lines
+          for an appointment. Book with us for a healthier lifestyle.
+        </div>
+        <div
+          className={styles.weCollectSamples}
+        >{`We collect samples timely to prevent treatment delays and ensure your continued well-being.  `}</div>
+        <div className={styles.enjoyHassleFree}>
+          {" "}
+          Enjoy hassle free medical assistance with our transparent approach. We
+          prioritize transparency for your smooth medical journey.
+        </div>
+      </div>
+      <div className={styles.header}>
+        <div className={styles.headerChild} />
+        <div className={styles.component10}>
+          <img
+            className={styles.screenshot20230725165513RIcon}
+            alt=""
+            src="/screenshot-20230725-165513removebgpreview-1@2x.png"
+          />
+          <img
+            className={styles.nilLogoTransparent111}
+            alt=""
+            src="/nil-logo-transparent-1-11@2x.png"
+          />
+        </div>
+        <div className={styles.labTestsParent}>
+          <div className={styles.labTests2}>Lab tests</div>
+          <div className={styles.home}>Home</div>
+          <div className={styles.healthConditions}>Health conditions</div>
+          <div className={styles.medicines}>Medicines</div>
+        </div>
+        <div className={styles.headerItem} />
+        <div className={styles.headerInner} />
+        <div className={styles.component11}>
+          <div className={styles.component11Child} />
+          <div className={styles.playStore}>Play store</div>
+          <img
+            className={styles.component11Item}
+            alt=""
+            src="/rectangle-48@2x.png"
+          />
+        </div>
+        <div className={styles.forPatients}>For patients</div>
+        <div className={styles.forHospitals}>For hospitals</div>
+      </div>
+      <div className={styles.topPackagesParent}>
+        <div className={styles.topPackages}>
+          <span>{`Top `}</span>
+          <span className={styles.packages}>packages</span>
+        </div>
+        <img className={styles.groupChild} alt="" src="/vector-5.svg" />
+        <div className={styles.exploreOurWide}>
+          Explore our wide range of tests! We ensure that you don’t miss out on
+          any chance to keep yourself healthy.
+        </div>
+        <div className={styles.groupItem} />
+        <div className={styles.groupInner} />
+        <div className={styles.groupChild1} />
+        <div className={styles.basicFullBody}>Basic Full Body Checkup</div>
+        <div className={styles.numberOfTests}>Number of tests - 53</div>
+        <div className={styles.div5}>4.3</div>
+        <div className={styles.reviews}>( 75 Reviews )</div>
+        <div className={styles.advanceFullBody}>Advance Full Body Checkup</div>
+        <div className={styles.numberOfTests1}>Number of tests - 70</div>
+        <div className={styles.div6}>4.7</div>
+        <div className={styles.reviews1}>( 100 Reviews )</div>
+        <div className={styles.liverFunctionTest}>
+          Liver Function Test (LFT)
+        </div>
+        <div className={styles.numberOfTests2}>Number of tests - 89</div>
+        <div className={styles.div7}>4.3</div>
+        <div className={styles.reviews2}>( 43 Reviews )</div>
+        <div className={styles.ellipseDiv} />
+        <button style={{cursor:'pointer'}}> <img className={styles.arrowIcon} alt="" src="/arrow-1.svg" /></button>
+        <img className={styles.gradeIcon} alt="" src="/grade.svg" />
+        <img className={styles.gradeIcon1} alt="" src="/grade.svg" />
+        <img className={styles.gradeIcon2} alt="" src="/grade.svg" />
+        <button className={ `${styles.bookNow} ${styles.groupChild2}`}>Book now</button>
+        <button className={ `${styles.bookNow1} ${styles.groupChild3}`} >Book now</button>
+        <button className={`${styles.bookNow2} ${styles.groupChild4}`}>Book now</button>
+
+        <div className={styles.div8}>10,000</div>
+        <div className={styles.div9}>8,490</div>
+        <img
+          className={styles.currencyRupeeIcon2}
+          alt=""
+          src="/currency-rupee2.svg"
+        />
+        <div className={styles.mrp}>MRP:</div>
+        <div className={styles.groupChild5} />
+        <img
+          className={styles.currencyRupeeIcon3}
+          alt=""
+          src="/currency-rupee3.svg"
+        />
+        <div className={styles.div10}>12,000</div>
+        <div className={styles.div11}>10,000</div>
+        <img
+          className={styles.currencyRupeeIcon4}
+          alt=""
+          src="/currency-rupee4.svg"
+        />
+        <img
+          className={styles.currencyRupeeIcon5}
+          alt=""
+          src="/currency-rupee5.svg"
+        />
+        <div className={styles.groupChild6} />
+        <div className={styles.mrp1}>MRP:</div>
+        <div className={styles.div12}>13,299</div>
+        <div className={styles.div13}>11,299</div>
+        <img
+          className={styles.currencyRupeeIcon6}
+          alt=""
+          src="/currency-rupee6.svg"
+        />
+        <div className={styles.groupChild7} />
+        <img
+          className={styles.currencyRupeeIcon7}
+          alt=""
+          src="/currency-rupee2.svg"
+        />
+        <div className={styles.mrp2}>MRP:</div>
+      </div>
+      <div className={styles.weKeepExpanding}>
+        <div className={styles.weKeepExpandingContainer}>
+          <span>{`We keep `}</span>
+          <span className={styles.packages}>expanding</span>
+          <span>!</span>
+        </div>
+        <img
+          className={styles.weKeepExpandingChild}
+          alt=""
+          src="/vector-5.svg"
+        />
+        <div className={styles.component15Parent}>
+          <div className={styles.component15}>
+            <div className={styles.component15Child} />
+            <div className={styles.div14}>5+</div>
+            <div className={styles.labs}>Labs</div>
+          </div>
+          <div className={styles.component16}>
+            <div className={styles.component15Child} />
+            <div className={styles.div15}>100+</div>
+            <div className={styles.labs1}>Labs</div>
+          </div>
+          <div className={styles.div16}>100+</div>
+          <div className={styles.collectionCenters}>Collection centers</div>
+          <div className={styles.component17}>
+            <div className={styles.component15Child} />
+            <div className={styles.div15}>5+</div>
+            <div className={styles.labs1}>Labs</div>
+          </div>
+          <div className={styles.div18}>1000+</div>
+          <div className={styles.monthlyTests}>Monthly tests</div>
+          <div className={styles.component18}>
+            <div className={styles.component15Child} />
+            <div className={styles.div15}>5+</div>
+            <div className={styles.labs1}>Labs</div>
+          </div>
+          <div className={styles.div20}>2200</div>
+          <div className={styles.testMenu}>Test menu</div>
+          <div className={styles.component19}>
+            <div className={styles.component15Child} />
+            <div className={styles.div15}>5+</div>
+            <div className={styles.labs1}>Labs</div>
+          </div>
+          <div className={styles.div22}>5+</div>
+          <div className={styles.citiesPresentIn}>Cities present in</div>
+        </div>
+      </div>
+      <div className={styles.accreditation}>
+        <img
+          className={styles.accreditationChild}
+          alt=""
+          src="/rectangle-81@2x.png"
+        />
+        <div className={styles.freeSampleCollection}>
+          Free sample collection
+        </div>
+        <img
+          className={styles.accreditationItem}
+          alt=""
+          src="/rectangle-80@2x.png"
+        />
+        <div className={styles.nablAccreditedLabs1}>NABL Accredited Labs</div>
+      </div>
+    </div>
+  );
+};
+
+export default LabPage1;
